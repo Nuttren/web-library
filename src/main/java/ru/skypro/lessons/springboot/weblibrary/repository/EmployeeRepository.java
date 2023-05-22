d87e4272f9398f75ea69a4f7c0841ea6e0dc0150
nuttren <roman.karpencko@gmail.com>
2023-05-22 12:20:57 +0400

    Spring Boot и MVC

--- src/main/java/ru/skypro/lessons/springboot/weblibrary/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.skypro.lessons.springboot.weblibrary.repository;
-
-import pojo.Employee;
-
-import java.util.List;
-
-public interface EmployeeRepository {
-    public List<Employee> getAllEmployees();
-}
