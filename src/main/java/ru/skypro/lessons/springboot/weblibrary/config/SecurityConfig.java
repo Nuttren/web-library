cc834b60851471ac26e8bf8e39ca66f1c190a3a2
nuttren <roman.karpencko@gmail.com>
2023-06-19 12:06:57 +0400

    Spring Boot и MVC

--- src/main/java/ru/skypro/lessons/springboot/weblibrary/config/SecurityConfig.java
+++ src/main/java/ru/skypro/lessons/springboot/weblibrary/config/SecurityConfig.java
@@ -5,22 +5,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.skypro.lessons.springboot.weblibrary.service.UserService;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserService userService;
 
-
+@Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService)
                 .passwordEncoder(passwordEncoder());
     }
 
-
+@Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/api/admin/**").hasRole("ADMIN")
